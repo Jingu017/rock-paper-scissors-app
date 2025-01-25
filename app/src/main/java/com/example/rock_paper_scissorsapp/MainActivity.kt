5192961c3b8972061fe9f179f49434873924daeb
Jingu017 <jingu.eng.pg495@gmail.com>
2025-01-25 20:22:18 +0900

    PlayerViewコンポーザブルを作成する

--- app/src/main/java/com/example/rock_paper_scissorsapp/MainActivity.kt
+++ app/src/main/java/com/example/rock_paper_scissorsapp/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rock_paper_scissorsapp.ui.theme.RockpaperscissorsAppTheme
 
@@ -44,4 +46,12 @@ fun GreetingPreview() {
     RockpaperscissorsAppTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun PlayerView(hand: Int) {
+    Image(
+        painter = painterResource(id = R.drawable.gu),
+        contentDescription = null
+    )
 }
